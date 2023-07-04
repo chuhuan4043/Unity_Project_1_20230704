8e65bc1d8954a4d8b7b7e3815e0589758fd07b1f
chuhuan4043 <138540202+chuhuan4043@users.noreply.github.com>
2023-07-04 16:09:21 +0800

    Update 發布WebGL、修正錯誤

--- Assets/A.cs
+++ Assets/A.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-int lv = 1;
 public class A : MonoBehaviour
 {
+    int lv = 1;
     // Start is called before the first frame update
     void Start()
     {
