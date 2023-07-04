ef8a66b465fc64e24aedc4e807b2e400431aa5cc
chuhuan4043 <138540202+chuhuan4043@users.noreply.github.com>
2023-07-04 14:08:36 +0800

    Update 編輯等級資料

--- Assets/A.cs
+++ Assets/A.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+int lv = 1;
 public class A : MonoBehaviour
 {
     // Start is called before the first frame update
